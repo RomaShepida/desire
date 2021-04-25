ff8b10029bed9c386e5328f82307554afed9dd80
Roma Shepida <roma.shepida@gmail.com>
2021-04-25 21:23:36 +0300

    page BLOG done (slider and pagination), started page BLOG-ONE (single page)

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ $(function () {
     $('.header__btn').on('click', function () {
         $('.rightside-menu').removeClass('rightside-menu--close');
     });
+
     $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
@@ -20,6 +22,11 @@ $(function () {
         arrows: false,
         slidesToShow: 10,
         slidesToScroll: 10
+    });
+
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-prev"><img src="../images/blog/left-slider-arrow.svg" alt="left arrow"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-next"><img src="../images/blog/right-slider-arrow.svg" alt="right arrow"></button>'
     });
 
     var mixer = mixitup('.gallery__inner', {
